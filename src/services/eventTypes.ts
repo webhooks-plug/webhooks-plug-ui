a16ffeabd79b4e990d5a0cc0a259bd51d4d52e02
davetech <davidrums65@gmail.com>
2024-01-24 22:22:00 +0100

    feat(services): add event types services

---
+++ src/services/eventTypes.ts
@@ -0,0 +1,14 @@
+import { notify } from "@/helpers/toast";
+import webplug from "@/lib/webplug";
+
+const createEventType = async (name: string, serviceId: string) => {
+  const response = await webplug.eventTypes.create(name, serviceId);
+  return response;
+};
+
+const fetchEventTypes = async (serviceId: string) => {
+  const response = await webplug.eventTypes.all(serviceId);
+  return response;
+};
+
+export { createEventType, fetchEventTypes };
