3a72cd9084f2c15bf5020f99ca54fa47220c3588
davetech <davidrums65@gmail.com>
2024-01-25 23:00:10 +0100

    feat(lib): add util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,18 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+const isHttps = (url: string) => {
+  const httpOrHttpsRegex = /^https?:\/\//;
+  return httpOrHttpsRegex.test(url);
+};
+
+export const validateUrl = (url: string) => {
+  const urlRegex = isHttps(url)
+    ? /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/
+    : /^(http?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+  return urlRegex.test(url);
+};
