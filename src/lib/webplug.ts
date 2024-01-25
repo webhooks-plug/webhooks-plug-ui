9eca70168d865e3a4ece682da030ac8f6b0c7f62
davetech <davidrums65@gmail.com>
2024-01-25 22:59:46 +0100

    feat(lib): add weplug config

---
+++ src/lib/webplug.ts
@@ -0,0 +1,8 @@
+import WebPlug from "webplug";
+
+const webplug = new WebPlug({
+  apiKey: "VjSRxh0PFV2PLlh5ZHzJr2gPQO8jVG3xaT10MLlW",
+  url: "https://548x2cznhi.execute-api.us-east-1.amazonaws.com/v1",
+});
+
+export default webplug;
