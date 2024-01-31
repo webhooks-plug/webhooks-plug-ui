88e8ccee8562707809fc88ae5449275e4b28571c
davetech <davidrums65@gmail.com>
2024-01-31 21:41:06 +0100

    feat(layout): add app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import routes from "@/routes/routes";
 import { useRoutes } from "react-router-dom";
+import AppLayout from "./components/shared/layout";
 
 function App() {
   const content = useRoutes(routes);
 
-  return <div>{content}</div>;
+  return (
+    <div>
+      <AppLayout>{content}</AppLayout>
+    </div>
+  );
 }
 
 export default App;
