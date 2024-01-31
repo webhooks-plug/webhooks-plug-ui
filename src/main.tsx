a48e868e9dbbc52cf31905ab3881c758ade9d916
davetech <davidrums65@gmail.com>
2024-01-31 21:42:09 +0100

    refactor(store): remove redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 import "./index.css";
 import App from "./App";
 import React from "react";
-import { store } from "@/store/store";
-import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 
@@ -12,9 +10,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <App />
       <Toaster position="top-center" />
     </Router>
   </React.StrictMode>
