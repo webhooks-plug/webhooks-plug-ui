0f6dfc39e34e095f17eac868cbdc0bb8d61b1859
davetech <davidrums65@gmail.com>
2024-01-31 22:12:16 +0100

    refactor(dashboard): clean out dashboard file

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,26 +1,12 @@
-import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import webplug from "@/lib/webplug";
 import ServiceTab from "./components/serviceTab";
 import EventTypesTab from "./components/eventTypesTab";
 import UsersTab from "./components/usersTab";
 import MessagesTab from "./components/messageTab";
 
 const Dashboard = () => {
-  const createService = async () => {
-    try {
-      const user = await webplug.subscriptions.get(
-        "de11ff5e-22af-4320-818d-f037fc57e20d"
-      );
-      console.log(user);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
   return (
     <section>
-      {/* <Button onClick={createService}>Click here</Button> */}
       <div className="flex-1 space-y-4 p-8 pt-6">
         <div className="flex items-center justify-between space-y-2">
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
@@ -30,7 +16,6 @@ const Dashboard = () => {
             <TabsTrigger value="service">Service Info</TabsTrigger>
             <TabsTrigger value="event_types">Event Types</TabsTrigger>
             <TabsTrigger value="users">Users</TabsTrigger>
-            {/* <TabsTrigger value="events">Events</TabsTrigger> */}
             <TabsTrigger value="messages">Messages</TabsTrigger>
           </TabsList>
           <TabsContent value="service" className="space-y-4">
@@ -42,7 +27,6 @@ const Dashboard = () => {
           <TabsContent value="users" className="space-y-4">
             <UsersTab />
           </TabsContent>
-          {/* <TabsContent value="events" className="space-y-4"></TabsContent> */}
           <TabsContent value="messages" className="space-y-4">
             <MessagesTab />
           </TabsContent>
